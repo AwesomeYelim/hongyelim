cca72f875dad403ce38ea3063ce22ba5c6285fde
AwesomeYelim <uiop01900@gmail.com>
2023-08-01 16:14:13 +0900

    darkmode

--- src/app/components/common/heart.tsx
+++ src/app/components/common/heart.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { useEffect, useState } from "react";
+
+import { useState } from "react";
 import axios from "axios";
-import { getPost, Post } from "@/service/posts";
+import { Post } from "@/service/posts";
 import classNames from "classnames";
 import "./heart.scss";
 
