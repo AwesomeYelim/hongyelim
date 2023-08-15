d3f88e1d8d1786c7d503c6820cad28896081d25e
Awesomeyelim <uiop01900@gmail.com>
2023-08-15 10:25:15 +0900

    props🍒

--- src/app/memo/page.tsx
+++ src/app/memo/page.tsx
@@ -4,12 +4,10 @@ import { Memo } from "../components/Memo";
 import "./page.scss";
 
 export default async function MemoPage() {
-  const posts = await getPosts();
-
   return (
     <div className="memo_page">
       <h1>Memo for Memories</h1>
-      <Memo posts={posts} />
+      <Memo />
     </div>
   );
 }
