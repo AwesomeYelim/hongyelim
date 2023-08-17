416f8cbd85a43e5aba6e14ae445cc6c921e8a8e1
AwesomeYelim <uiop01900@gmail.com>
2023-08-17 10:09:53 +0900

    tag-style🍕

--- src/app/components/Tag.tsx
+++ src/app/components/Tag.tsx
@@ -5,8 +5,8 @@ import classNames from "classnames";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import { styled } from "styled-components";
 import LocalStorage from "./common/functions/localstorage";
-import "./Tag.scss";
 
 export type Selected = {
   keyword: string;
@@ -18,16 +18,44 @@ export interface Props {
   selected?: Selected;
   setSelected?: React.Dispatch<React.SetStateAction<Selected>>;
 }
+const TagWrap = styled.nav`
+  display: flex;
+  position: absolute;
+  flex-direction: column;
+  margin: 10px 0 10px 0;
+  padding-left: 20px;
+  top: 125px;
+  right: -130px;
+  width: 150px;
 
+  box-sizing: border-box;
+  p {
+    cursor: pointer;
+    margin: 5px 0 5px 0;
+    font-size: 15px;
+    border: 1px solid transparent;
+
+    &:first-child {
+      color: orange;
+      border-bottom: 1px solid black !important;
+      padding-bottom: 10px;
+      a {
+        font-weight: 700;
+      }
+    }
+
+    &.active {
+      font-weight: 600;
+    }
+  }
+`;
 export const Tag = ({ posts, selected, setSelected }: Props): JSX.Element => {
   const [list, setList] = useState<string[]>();
   const location = usePathname();
 
   const { tag, title } = {
     /** tag 별 list   */
-    tag: posts
-      .map((item: Post) => item.tag)
-      .filter((item, i, arr) => arr.indexOf(item) === i),
+    tag: posts.map((item: Post) => item.tag).filter((item, i, arr) => arr.indexOf(item) === i),
     /** title 별 list   */
     title: posts.map((item: Post) => item.title),
   };
@@ -48,7 +76,7 @@ export const Tag = ({ posts, selected, setSelected }: Props): JSX.Element => {
   }, [posts]);
 
   return (
-    <nav className="tag_wrap">
+    <TagWrap className="tag_wrap">
       {list?.map((keyword) => {
         return (
           <p
@@ -63,14 +91,12 @@ export const Tag = ({ posts, selected, setSelected }: Props): JSX.Element => {
                   posts: select,
                 });
 
-                if (keyword === "All")
-                  setSelected({ keyword, posts: [...posts] });
+                if (keyword === "All") setSelected({ keyword, posts: [...posts] });
                 if (e.currentTarget.innerText === "Recommand Title") {
                   setSelected({ keyword: "" });
                 }
               }
-            }}
-          >
+            }}>
             {list[0] === "Tag" ? (
               <Link
                 href="/posts"
@@ -78,8 +104,7 @@ export const Tag = ({ posts, selected, setSelected }: Props): JSX.Element => {
                   if (e.currentTarget.innerText !== "Tag") {
                     LocalStorage.setItem("tag", e.currentTarget.innerText);
                   }
-                }}
-              >
+                }}>
                 {keyword}
               </Link>
             ) : (
@@ -88,6 +113,6 @@ export const Tag = ({ posts, selected, setSelected }: Props): JSX.Element => {
           </p>
         );
       })}
-    </nav>
+    </TagWrap>
   );
 };
