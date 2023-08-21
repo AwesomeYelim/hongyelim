5c46eef1d869efa8d248cf85290bd93f9ab051e0
AwesomeYelim <uiop01900@gmail.com>
2023-08-21 16:48:44 +0900

    ✨

--- src/app/api/[id]/route.ts
+++ src/app/api/[id]/route.ts
@@ -5,10 +5,10 @@ import { PrismaClient } from "@prisma/client";
 export async function GET(req: Request, res: Response) {
   const prisma = new PrismaClient();
   // const user = await prisma.users.findMany();
-  const posts = await prisma.post.findMany();
+  // const posts = await prisma.post.findMany();
 
   const data = req.json();
   const post = await getPost((res as Response & { params: { id: string } }).params.id);
   // return NextResponse.json(posts);
-  return NextResponse.json(posts);
+  return NextResponse.json(post);
 }
