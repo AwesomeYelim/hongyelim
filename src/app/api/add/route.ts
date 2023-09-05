58dae2b17da2ed753a48fe5b6811ba68ab36496c
AwesomeYelim <uiop01900@gmail.com>
2023-09-05 18:56:28 +0900

    🌟

--- src/app/api/add/route.ts
+++ src/app/api/add/route.ts
@@ -15,26 +15,11 @@ export async function POST(req: Request, res: Response) {
 
   const mdPath = (() => {
     if (target) {
-      return path.join(
-        process.cwd(),
-        "data",
-        "md",
-        `${target?.id}_${target?.title}.md`
-      );
+      return path.join(process.cwd(), "data", "md", `${target?.id}_${target?.title}.md`);
     } else {
-      return path.join(
-        process.cwd(),
-        "data",
-        "md",
-        `${posts.length + 1}_${decodeURI(data.title)}.md`
-      );
+      return path.join(process.cwd(), "data", "md", `${posts.length + 1}_${decodeURI(data.title)}.md`);
     }
   })();
-
-  // const title = data.queryKey.split("_")[1];
-  console.log(data);
-
-  // const postData = collection(db, "posts");
 
   const postData = doc(db, "posts", data.title);
 
@@ -47,42 +32,18 @@ export async function POST(req: Request, res: Response) {
 
   if (!isMd) {
     mdFile = `\n\n${data.content}`;
-    // posts = [
-    //   ...posts,
-    //   {
-    //     id: posts.length + 1,
-    //     title: data.title.replace(/\s/g, ""),
-    //     content:
-    //       (data.content.match(/#+\s(.+)/g) &&
-    //         data.content.match(/#+\s(.+)/g)[0]) ||
-    //       data.content,
-    //     tag: [data.title],
-    //     post_title: "",
-    //     heart: {},
-    //     heart_count: 0,
-    //     created_at: Math.floor(new Date().getTime() / 1000),
-    //     comments: [],
-    //   },
-    // ];
-    console.log(postData);
 
     await setDoc(postData, {
       id: posts.length + 1,
       title: data.title.replace(/\s/g, ""),
-      content:
-        (data.content.match(/#+\s(.+)/g) &&
-          data.content.match(/#+\s(.+)/g)[0]) ||
-        data.content,
+      content: (data.content.match(/#+\s(.+)/g) && data.content.match(/#+\s(.+)/g)[0]) || data.content,
       tag: [data.title],
-      post_title: "",
+      post_title: data.title.replace(/\s/g, ""),
       heart: {},
       heart_count: 0,
       created_at: Math.floor(new Date().getTime() / 1000),
       comments: [],
     });
-    // fs.writeFile(filePath, JSON.stringify(posts, null, 3), (err) => {
-    //   NextResponse.json({ message: err });
-    // });
   } else {
     mdFile = fs.readFileSync(mdPath);
     mdFile = mdFile.toString() + `\n\n${data.content}`;
