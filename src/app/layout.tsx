0c1a2590bb9d31acd607817eb63f886bc7f46831
Awesomeyelim <uiop01900@gmail.com>
2023-09-17 23:18:19 +0900

    update_data🎱

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       <GoogleAnalytics />
       <head>
         <meta name="google-site-verification" content="2s1XEZhZCijhcLEsQR5t6LFthsxawdQRPmx15i9n_XI" />
-        <meta property="og:image" content="/images/main.jpg" />
+        <meta property="og:image" content="/public/images/main.jpg" />
         <meta httpEquiv="Expires" content="0" />
         <meta httpEquiv="Pragma" content="no-cache" />
         <script
