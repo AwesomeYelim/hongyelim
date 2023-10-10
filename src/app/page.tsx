1baaf2a80644e1042ea03fc22276e1d40fec9db3
AwesomeYelim <uiop01900@gmail.com>
2023-10-11 00:15:42 +0900

    fade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,24 @@ export default async function HomePage() {
   return (
     <div className="main_wrapper">
       <div className="svg_wrapper">
-      <div className="welcome_img">
+        <div className="welcome_img">
           <Welcome width={200} height={150} />
         </div>
         <div className="text_area">
           <p>Hello !</p>
-          <p>This is <span>Yelimblog.</span></p>
+          <p>
+            This is <span>Yelimblog.</span>
+          </p>
           <p>It `s time to learning new things !</p>
           <p className="upper">Just click it →</p>
         </div>
-        <Image className="dance" src={Dance} alt="dance" height={200} width={200} />
-      
+        <Image
+          className="dance"
+          src={Dance}
+          alt="dance"
+          height={200}
+          width={200}
+        />
       </div>
       <Tag />
     </div>
